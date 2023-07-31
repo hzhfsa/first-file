59ae228fd88db6ec8bd3609c0f534bc81cd46a62
hzhfsa <1957214281@qq.com>
2023-07-31 15:17:30 +0800

    first

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 v-show="Ispage">This is an about page</h1>
+    <button @click="changeIspage">change</button>
   </div>
 </template>
+
+<script setup>
+import bus from '@/utils/bus.js'
+import { ref } from 'vue';
+
+let Ispage = ref(true)
+function changeIspage(){
+  console.log('ccc');
+  Ispage.value = !Ispage.value;
+  console.log('Ispage:',Ispage.value);
+}
+
+bus.on('changestatus', () => {
+  console.log('123');
+  changeIspage()
+})
+</script>
